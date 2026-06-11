fa267e7b12745067d240aa49411073e53c5638ae
Victor.Romanatto <victorcesarromanatto@gmail.com>
2026-06-10 23:45:52 -0300

    cors add

--- src/main.py
+++ src/main.py
@@ -8,13 +8,18 @@ app = FastAPI(
     title="Encurtador-Url-API",
     description="API de serviça de encurtador de url",
     version="1.0.0",
+        servers=[
+        {"url": "http://localhost:8000"}
+    ]
+
 )
 
 app.add_middleware(
-    CORSMiddleware,
+    CORSMiddleware, 
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "http://127.0.0.1:8000/docs",
     ],
     allow_credentials=True,
     allow_methods=["*"],
